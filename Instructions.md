e02e486fa296e9ecd1c0ede47bf74897804c5c15
petarcorluka86 <petarcorluka86@gmail.com>
2025-04-04 15:31:45 +0200

    cleanup

---
+++ Instructions.md
@@ -0,0 +1,14 @@
+# 1. Project setup and overview
+
+### Installation
+
+- Command: `npx create-next-app@latest`
+- More about installation: https://nextjs.org/docs/app/getting-started/installation
+
+### Project overview
+
+- Cleanup
+  - favicon.ico
+- More about project structure: https://nextjs.org/docs/app/getting-started/project-structure
+
+###
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bubblegum_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const font = Bubblegum_Sans({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -24,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
